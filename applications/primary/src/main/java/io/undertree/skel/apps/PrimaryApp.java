2a0efc1f3476d9ba55f1a376e07c95257ff10023
Shawn Sherwood <shawn.sherwood@gmail.com>
2017-10-22 13:50:46 -0400

    Minor cleanup to show example with boot plugin only on applications

--- applications/primary/src/main/java/io/undertree/skel/apps/PrimaryApp.java
+++ applications/primary/src/main/java/io/undertree/skel/apps/PrimaryApp.java
@@ -3,6 +3,7 @@ package io.undertree.skel.apps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
--- applications/primary/src/test/java/io/undertree/skel/apps/HelloControllerIntTests.java
+++ applications/primary/src/test/java/io/undertree/skel/apps/HelloControllerIntTests.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = HelloController.class)
+@ComponentScan({ "io.undertree.hello" })
 public class HelloControllerIntTests {
 
 	@Autowired
