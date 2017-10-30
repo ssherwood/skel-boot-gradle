c34ea69f1e931351bbbce372f9ea83fed58b6371
Shawn Sherwood <shawn.sherwood@kroger.com>
2017-10-30 09:32:24 -0400

    Minor tweak to Kotlin annotation config

--- components/goodbye/src/main/java/io/undertree/goodbye/EnableGoodbyeComponent.kt
+++ components/goodbye/src/main/java/io/undertree/goodbye/EnableGoodbyeComponent.kt
@@ -1,11 +1,9 @@
 package io.undertree.goodbye
 
 import org.springframework.context.annotation.Import
-import java.lang.annotation.Inherited
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
-@Inherited
 @Import(GoodbyeConfig::class)
 annotation class EnableGoodbyeComponent
